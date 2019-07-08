7d805369b140aff466952c700479cdb1a00f5eb5
David Whaley <davidwhaleycode@outlook.com>
2019-07-08 12:44:06 -0500

    'initsetup'

---
+++ src/js/sceneCreate.js
@@ -0,0 +1,19 @@
+(funtion() {
+    'use strict';
+    //link to, or create, namespace
+    window.sceneNS = window.sceneNS || {};
+    //scene creation function
+    window.sceneNS.sceneCreate = 
+    /**
+     * Create a Scene instance and return it
+     * 
+@param  {Canvas} canvas [canvas element which will display the new Scene]
+     * 
+@param  {Engine} engine [Engine class instance used to create the Scene]
+     * 
+@return {Scene}         [newly created Scene instance]
+     */
+    function sceneCreate(canvas, engine) {
+        //convenience alias for Babylon
+    }
+}
